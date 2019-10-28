5f79a3c87df1c5ce83f5f8ac6fd6bfa07c93e677
Shrirang Patil <shrirangpatil1996@gmail.com>
2019-10-29 00:10:05 +0530

    Polishing the layouts

--- app/src/main/java/com/example/android/myexpensemanager/MainActivity.java
+++ app/src/main/java/com/example/android/myexpensemanager/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String about = "All the data gets stored locally on the device. Happy Privacy!";
+        TextView aboutTextView = findViewById(R.id.mxm_about);
+        aboutTextView.setText(about);
         Button addButton = findViewById(R.id.mxm_add);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
