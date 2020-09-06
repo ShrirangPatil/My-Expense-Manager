525a4c7d17e79fa5642f68e34622a33f94587925
ShrirangPatil <shrirangpatil1996@outlook.com>
2020-09-06 22:54:51 +0530

    Modified the main activity layout

--- app/src/main/java/com/example/android/myexpensemanager/MainActivity.java
+++ app/src/main/java/com/example/android/myexpensemanager/MainActivity.java
@@ -4,8 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,10 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String about = "All the data gets stored locally on the device. Happy Privacy!";
-        TextView aboutTextView = findViewById(R.id.mxm_about);
-        aboutTextView.setText(about);
-        Button addButton = findViewById(R.id.mxm_add);
+        ImageButton addButton = findViewById(R.id.mxm_add);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button searchButton = findViewById(R.id.mxm_search);
+        ImageButton searchButton = findViewById(R.id.mxm_search);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final Button deleteButton = findViewById(R.id.mxm_delete);
+        ImageButton deleteButton = findViewById(R.id.mxm_delete);
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
